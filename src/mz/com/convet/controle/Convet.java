dfee89c5f7f85d8fc630f00810bdce68393a4f56
Miguel <miguelmmanjate@gmail.com>
2016-08-29 16:34:19 +0200

    inicio do projeto

---
+++ src/mz/com/convet/controle/Convet.java
@@ -0,0 +1,101 @@
+package mz.com.convet.controle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/adiciona")
+public class Convet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest request, 
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		String  numeroString = request.getParameter("natural");
+		int vlr = Integer.parseInt(numeroString);
+		//int sobra = 0;
+		String roma ="";	
+		if(vlr < 4000){
+			
+			while(vlr >= 1000){
+				roma +="M";
+				vlr -= 1000;
+			}
+			
+			while(vlr >= 900){
+				roma += "CM";
+				vlr -= 900;
+			}
+			while(vlr >= 500){
+				roma +="D";
+				vlr -= 500;
+			}
+			while(vlr >= 400){
+				roma += "CD";
+				vlr -= 400;
+			}
+			while(vlr >= 100){
+				roma +="C";
+				vlr -=100;
+			}
+			while(vlr >= 90){
+				roma +="XC";
+				vlr -= 90;
+			}
+			while(vlr >= 50){
+				roma +="L";
+				vlr -= 50;
+			}
+			while(vlr >= 40){
+				roma +="XL";
+				vlr -= 40;
+			}
+			while(vlr >= 10){
+				roma += "X";
+				vlr -= 10;
+			}
+			 while (vlr >= 9) {
+	                roma += "IX";
+	                vlr -= 9;
+	            }
+	            
+	            while (vlr >= 5) {
+	                roma += "V";
+	                vlr -= 5;
+	            }
+	           
+	            while (vlr >= 4) {
+	                roma += "IV";
+	                vlr -= 4;
+	            }
+	   
+	            while (vlr >= 1) {
+	                roma += "I";
+	                vlr -= 1;
+	            }
+	            System.out.println("O Algarismo Romano correspondente a " + " é " + roma);
+	        } else {
+	            System.out.print("Valor Inválido!");
+			
+		}
+		
+	request.setAttribute("romano", roma);
+		RequestDispatcher rd = request.getRequestDispatcher("/romanos.jsp");
+		rd.forward(request, response);
+
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
